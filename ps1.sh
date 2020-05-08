cbb4e5167690a9e7784ec46ab0b7cae737f41928
Is Ma <dotpointing@gmail.com>
2020-05-08 02:24:51 -0500

    Style, chose a mini diamon, refactor function

--- ps1.sh
+++ ps1.sh
@@ -1,15 +1,12 @@
 function is_green (){
   if [[ -d ".git" ]]  # is there a .git/ directory here?
   then
-
     # is my working directory clean?
-    if [ -z "$(git status --porcelain)" ]; then
-      return 0;  # green
-    else
-      return -1;  # red
-    fi
-
+    if [ -z "$(git status --porcelain)" ]; then return 0; fi;  # green
+    return -1;  # red
   fi
 }
 
-export PS1="${txtblu}[ ${bldred}\T${txtrst}${txtblu} | ${bldylw}\u@\H ${txtblu}| ${bldgrn}\w${txtblu} ] ${txtblk}\n\$(if is_green; then echo '\e[42m'; else echo '\e[41m'; fi)\$(__git_ps1 "%s")${txtrst} > "
+PS1="${bldred}\T${txtrst}${txtpur}|${bldylw}\H${txtpur}|${bldgrn}\w${txtpur}|"
+PS1="$PS1${txtblu}\$(__git_ps1 "%s") \$(if is_green; then echo '\e[1;32m'; else echo '\e[1;31m'; fi)⧫${txtrst}\n"
+export PS1
