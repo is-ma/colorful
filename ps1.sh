e7f4477d032a3aa14a2f3f267df150384b90fdc0
Is Ma <dotpointing@gmail.com>
2020-05-07 20:38:15 -0500

    Add color (green/red) to the current git branch on prompt PS1

--- ps1.sh
+++ ps1.sh
@@ -1,1 +1,15 @@
-export PS1="${txtblu}[ ${bldred}\T${txtrst}${txtblu} | ${bldylw}\u@\H ${txtblu}| ${bldgrn}\w${bldpur}\$(__git_ps1) ${txtrst}${txtblu}] ${txtwht}\n"
+function is_green (){
+  if [[ -d ".git" ]]  # is there a .git/ directory here?
+  then
+
+    # is my working directory clean?
+    if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+      return 0;  # green
+    else
+      return -1;  # red
+    fi
+
+  fi
+}
+
+export PS1="${txtblu}[ ${bldred}\T${txtrst}${txtblu} | ${bldylw}\u@\H ${txtblu}| ${bldgrn}\w${txtblu} ] ${txtblk}\n\$(if is_green; then echo '\e[42m'; else echo '\e[41m'; fi)\$(__git_ps1 "%s")${txtrst} > "
