33eda76bbce0fc5af8fe52ea067c204d5cffeccf
Is Ma <dotpointing@gmail.com>
2023-05-19 11:36:19 -0600

    Add silent source

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -5,3 +5,6 @@ source ~/.is-ma/colorful/mods/git_prompt.sh
 source ~/.is-ma/colorful/mods/bash_colors.sh
 source ~/.is-ma/colorful/mods/ps1.sh
 EOF
+
+# silent source
+source ~/.bashrc > /dev/null 2>&1
